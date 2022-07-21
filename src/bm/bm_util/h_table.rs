a7c9324a7042c259b91814bc936998f4fa4874df
analog-hors <44050761+analog-hors@users.noreply.github.com>
2022-07-21 16:27:27 +1000

    Clean up history tables

--- src/bm/bm_util/h_table.rs
+++ src/bm/bm_util/h_table.rs
@@ -1,89 +1,95 @@
 use cozy_chess::{Board, Color, Move, Piece, Square};
 
 pub const MAX_VALUE: i32 = 512;
-const SQUARE_COUNT: usize = 64;
-const PIECE_COUNT: usize = 12;
+
+/// From-to table
+type ButterflyTable<T> = [[T; Square::NUM]; Square::NUM];
+fn butterfly_table<T: Copy>(value: T) -> ButterflyTable<T> {
+    [[value; Square::NUM]; Square::NUM]
+}
+
+type PieceToSquareTable<T> = [[T; Square::NUM]; Piece::NUM];
+fn piece_to_square_table<T: Copy>(value: T) -> PieceToSquareTable<T> {
+    [[value; Square::NUM]; Piece::NUM]
+}
 
 #[derive(Debug, Clone)]
 pub struct HistoryTable {
-    table: Box<[[i16; SQUARE_COUNT]; SQUARE_COUNT * 2]>,
+    table: Box<[ButterflyTable<i16>; Color::NUM]>,
 }
 
 impl HistoryTable {
     pub fn new() -> Self {
         Self {
-            table: Box::new([[0_i16; SQUARE_COUNT]; SQUARE_COUNT * 2]),
+            table: Box::new([butterfly_table(0); Color::NUM]),
         }
     }
 
     pub fn get(&self, color: Color, from: Square, to: Square) -> i16 {
-        let from_index = sq_index(color, from);
-        let to_index = to as usize;
-        self.table[from_index][to_index]
+        self.table[color as usize][from as usize][to as usize]
+    }
+
+    fn get_mut(&mut self, color: Color, from: Square, to: Square) -> &mut i16 {
+        &mut self.table[color as usize][from as usize][to as usize]
     }
 
     pub fn cutoff(&mut self, board: &Board, make_move: Move, fails: &[Move], amt: u32) {
-        let index = sq_index(board.side_to_move(), make_move.from);
-        let to_index = make_move.to as usize;
+        let color = board.side_to_move();
 
-        let value = self.table[index][to_index];
+        let value = self.get(color, make_move.from, make_move.to);
         let change = (amt * amt) as i16;
         let decay = (change as i32 * value as i32 / MAX_VALUE) as i16;
 
         let increment = change - decay;
 
-        self.table[index][to_index] += increment;
+        *self.get_mut(color, make_move.from, make_move.to) += increment;
 
         for &quiet in fails {
-            let index = sq_index(board.side_to_move(), quiet.from);
-            let to_index = quiet.to as usize;
-            let value = self.table[index][to_index];
+            let value = self.get(color, quiet.from, quiet.to);
             let decay = (change as i32 * value as i32 / MAX_VALUE) as i16;
             let decrement = change + decay;
 
-            self.table[index][to_index] -= decrement;
+            *self.get_mut(color, quiet.from, quiet.to) -= decrement;
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct CounterMoveTable {
-    table: Box<[[Option<Move>; SQUARE_COUNT]; PIECE_COUNT]>,
+    table: Box<[PieceToSquareTable<Option<Move>>; Color::NUM]>,
 }
 
 impl CounterMoveTable {
     pub fn new() -> Self {
         Self {
-            table: Box::new([[None; SQUARE_COUNT]; PIECE_COUNT]),
+            table: Box::new([piece_to_square_table(None); Color::NUM]),
         }
     }
 
     pub fn get(&self, color: Color, piece: Piece, to: Square) -> Option<Move> {
-        let piece_index = piece_index(color, piece);
-        let to_index = to as usize;
-        self.table[piece_index][to_index]
+        self.table[color as usize][piece as usize][to as usize]
     }
 
     pub fn cutoff(&mut self, board: &Board, prev_move: Move, cutoff_move: Move, amt: u32) {
         if amt > 20 {
             return;
         }
-        let piece = board.piece_on(prev_move.to).unwrap_or(Piece::King);
-        let piece_index = piece_index(board.side_to_move(), piece);
-        let to_index = prev_move.to as usize;
-        self.table[piece_index][to_index] = Some(cutoff_move);
+        let to = prev_move.to;
+        let color = board.side_to_move();
+        let piece = board.piece_on(to).unwrap_or(Piece::King);
+        self.table[color as usize][piece as usize][to as usize] = Some(cutoff_move);
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct DoubleMoveHistory {
-    table: Box<[[[[i16; SQUARE_COUNT]; PIECE_COUNT / 2]; SQUARE_COUNT]; PIECE_COUNT]>,
+    table: Box<[PieceToSquareTable<PieceToSquareTable<i16>>; Color::NUM]>,
 }
 
 impl DoubleMoveHistory {
     pub fn new() -> Self {
         Self {
-            table: Box::new([[[[0; SQUARE_COUNT]; PIECE_COUNT / 2]; SQUARE_COUNT]; PIECE_COUNT]),
+            table: Box::new([piece_to_square_table(piece_to_square_table(0)); Color::NUM]),
         }
     }
 
@@ -95,11 +101,19 @@ impl DoubleMoveHistory {
         piece_1: Piece,
         to_1: Square,
     ) -> i16 {
-        let piece_0_index = piece_index(color, piece_0);
-        let to_0_index = to_0 as usize;
-        let piece_1_index = piece_1 as usize;
-        let to_1_index = to_1 as usize;
-        self.table[piece_0_index][to_0_index][piece_1_index][to_1_index]
+        self.table[color as usize][piece_0 as usize][to_0 as usize][piece_1 as usize][to_1 as usize]
+    }
+
+    fn get_mut(
+        &mut self,
+        color: Color,
+        piece_0: Piece,
+        to_0: Square,
+        piece_1: Piece,
+        to_1: Square,
+    ) -> &mut i16 {
+        &mut self.table[color as usize][piece_0 as usize][to_0 as usize][piece_1 as usize]
+            [to_1 as usize]
     }
 
     pub fn cutoff(
@@ -110,39 +124,26 @@ impl DoubleMoveHistory {
         fails: &[Move],
         amt: u32,
     ) {
+        let color = board.side_to_move();
+
         let prev_piece = board.piece_on(prev_move.to).unwrap_or(Piece::King);
-        let prev_index = piece_index(board.side_to_move(), prev_piece);
-        let prev_to_index = prev_move.to as usize;
-
         let piece = board.piece_on(make_move.from).unwrap();
-        let index = piece as usize;
-        let to_index = make_move.to as usize;
 
-        let value = self.table[prev_index][prev_to_index][index][to_index];
+        let value = self.get(color, prev_piece, prev_move.to, piece, make_move.to);
         let change = (amt * amt) as i16;
         let decay = (change as i32 * value as i32 / MAX_VALUE) as i16;
 
         let increment = change - decay;
 
-        self.table[prev_index][prev_to_index][index][to_index] += increment;
+        *self.get_mut(color, prev_piece, prev_move.to, piece, make_move.to) += increment;
 
         for &quiet in fails {
             let piece = board.piece_on(quiet.from).unwrap();
-            let index = piece as usize;
-            let to_index = quiet.to as usize;
-            let value = self.table[prev_index][prev_to_index][index][to_index];
+            let value = self.get(color, prev_piece, prev_move.to, piece, quiet.to);
             let decay = (change as i32 * value as i32 / MAX_VALUE) as i16;
             let decrement = change + decay;
 
-            self.table[prev_index][prev_to_index][index][to_index] -= decrement;
+            *self.get_mut(color, prev_piece, prev_move.to, piece, quiet.to) -= decrement;
         }
     }
-}
-
-fn piece_index(color: Color, piece: Piece) -> usize {
-    color as usize * PIECE_COUNT / 2 + piece as usize
-}
-
-fn sq_index(color: Color, sq: Square) -> usize {
-    color as usize * SQUARE_COUNT + sq as usize
 }
